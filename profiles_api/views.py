dd81fb0eba9d7f4824ad46c764d20531d091854a
rohan <rohanrajgautam@gmail.com>
2020-10-06 19:14:07 +0545

    created our first APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.view import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+  '''Test API view'''
+
+  def get(self, request, format=None):
+    """List of API VIEW """
+    an_apiview = [
+      'rohan',
+      'rohit',
+      'khem',
+      'babita',
+    ]
+
+    return Response({'message':'Hello!', 'an_apiview':an_apiview})
