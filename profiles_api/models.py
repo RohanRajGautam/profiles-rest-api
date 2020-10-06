b7a19b619c9fbc75868eff3a10cfc3bb6e87d8d8
rohan <rohanrajgautam@gmail.com>
2020-10-06 17:38:26 +0545

    create user and superuser profile manager

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,7 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
+
+class UserProfileManager(BaseUserManager):
+  """Manager for user profile"""
+
+  def create_user(self, email, name, password=None):
+    """Create a new user profile"""
+    if not email:
+      raise ValueError('User must have an email address')
+
+    email = self.normalize_email(email)
+    user = self.model(email=email, name=name)
+
+    user.set_password(password)
+    user.save(using=self._db)
+
+    return user
+
+  def create_superuser(self, email, name, password):
+    """create and save a new superuser"""
+    user = self.create_user(email, name, password)
+
+    user.is_superuser = True
+    user.is_staff = True
+    user.save(using=self._db)
+
+    return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
