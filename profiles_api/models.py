a190729243f517aa69114b8141df15a25133f65f
rohan <rohanrajgautam@gmail.com>
2020-10-09 19:45:40 +0545

    migrated data of profilefeeditem

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -31,28 +31,28 @@ class UserProfileManager(BaseUserManager):
     return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database model for users in the system"""
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+  """Database model for users in the system"""
+  email = models.EmailField(max_length=255, unique=True)
+  name = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
 
-    objects = UserProfileManager()
+  objects = UserProfileManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['name']
 
-    def get_full_name(self):
-        """Retrieve full name for user"""
-        return self.name
+  def get_full_name(self):
+    """Retrieve full name for user"""
+    return self.name
 
-    def get_short_name(self):
-        """Retrieve short name of user"""
-        return self.name
+  def get_short_name(self):
+    """Retrieve short name of user"""
+    return self.name
 
-    def __str__(self):
-        """Return string representation of user"""
-        return self.email
+  def __str__(self):
+    """Return string representation of user"""
+    return self.email
 
 
 class ProfileFeedItem(models.Model):
