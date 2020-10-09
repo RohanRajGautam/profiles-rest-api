f8d09f444dbb198339992ab2aa113b72bc1035a0
rohan <rohanrajgautam@gmail.com>
2020-10-09 20:22:52 +0545

    added permission of profile api to UserProfileFeedViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from profiles_api import serializers, models, permissions
 
@@ -112,6 +113,10 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
   authentication_classes = (TokenAuthentication,)
   serializer_class = serializers.ProfileFeedItemSerializer
   queryset = models.ProfileFeedItem.objects.all()
+  permission_classes = (
+    permissions.UpdateOwnStatus,
+    IsAuthenticatedOrReadOnly
+  )
 
   def perform_create(self, serializer):
     serializer.save(user_profile=self.request.user)
